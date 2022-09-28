a6c4ce091ca37181b5400677789dfb8b16a3ca58
Nilo Freitas <nilo.n2m15@gmail.com>
2022-09-28 17:42:10 -0300

    finalizando o app github-explorer

--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -4,8 +4,14 @@ import RepositoryItem from "../RepositoryItem";
 
 import '../../styles/repositories.scss';
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export default function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -23,7 +29,7 @@ export default function RepositoryList() {
     
       <ul>
         {repositories.map(repository => {
-          return <RepositoryItem key={repository.id} repository={repository} />;
+          return <RepositoryItem key={repository.name} repository={repository} />;
         })}
       </ul>
     </section>
